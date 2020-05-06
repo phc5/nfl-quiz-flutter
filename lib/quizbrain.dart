1a97aed46a68c585738d11476edc369eab6ef98d
Paul Chong <paulhyunchong@gmail.com>
2020-05-05 22:31:19 -0700

    Adds more questions

--- lib/quizbrain.dart
+++ lib/quizbrain.dart
@@ -5,9 +5,27 @@ class QuizBrain {
   int _currentQuestion = 0;
 
   List<Question> _questions = [
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true)
+    Question(
+        'It takes over 3,000 cows to supply the NFL with enough leather for a year\'s supply of footballs.',
+        true),
+    Question('Miami has hosted the most Superbowls (10).', true),
+    Question('Tom Brady plays for the Tampa Bay Buccaneers.', true),
+    Question(
+        'There have been 4 instances in NFL history where a team has pulled off back-to-back championships.',
+        false),
+    Question(
+        'New England alone holds the NFL record for most Super Bowls.', false),
+    Question('Drew Brees has won the regular season MVP award.', false),
+    Question('Adrian Peterson is the NFL all-time leading rusher.', false),
+    Question(
+        'Michael Vick was taken with the 1st pick in the NFL Draft by the Atlanta Falcons.',
+        true),
+    Question(
+        'Four Heisman Trophy winners have gone on to be MVP of the Super Bowl.',
+        true),
+    Question(
+        'Texas has produced more pro football Hall of Famers than any other state.',
+        false)
   ];
 
   void getNextQuestion() {
